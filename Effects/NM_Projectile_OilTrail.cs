a146290ca3d2ad65e9f44ba3ef07cd03e64fd676
Hans Agerbo <hansahj@gmail.com>
2022-04-22 11:42:45 +0200

    Toad Oil implemented

--- Effects/NM_Projectile_OilTrail.cs
+++ Effects/NM_Projectile_OilTrail.cs
@@ -6,18 +6,16 @@ namespace NarutoMod.Effects
 {
     public class NM_Projectile_OilTrail : Projectile_Explosive
     {
-        private int TicksforAppearence = 1;
+        private int TicksforAppearence = 3;
 
         public override void Tick()
         {
             base.Tick();
-            def = RimWorld.ThingDefOf.Filth_Fuel;
             TicksforAppearence--;
             if (TicksforAppearence == 0 && Map != null)
             {
-                SmokeThrowher.ThrowSmokeTrail(Position.ToVector3Shifted(), 0.1f, Map, "NM_Mote_OiltrailSoft");
-                FilthMaker.TryMakeFilth(DestinationCell, Map, def, 1);
-                TicksforAppearence = 1;
+                SmokeThrowher.ThrowSmokeTrail(Position.ToVector3Shifted(), 0.4f, Map, "NM_Mote_OiltrailSoft");
+                TicksforAppearence = 3;
             }
         }
     }
